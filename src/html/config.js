a61b582bc1ca8bd7ba560b67da1e8e7df9355e17
Kazuhiro Yoshida <sy250f@gmail.com>
2019-02-11 21:13:44 +0900

    change config.js

--- src/html/config.js
+++ src/html/config.js
@@ -3,6 +3,16 @@ jQuery.noConflict();
     "use strict";
     // window.addEventListener('DOMContentLoaded', function() {
     $(document).ready(function() {
+
+        function escapeHtml(htmlstr) {
+            return htmlstr
+                .replace(/&/g, '&amp;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;')
+                .replace(/'/g, '&quot;')
+                .replace(/'/g, '&#39;');
+        }
+
         console.log(kintone.plugin.app.getConfig(pluginId));
         // let config = kintone.plugin.app.getConfig(pluginId);
         // if (typeof (config['name']) !== 'undefined') {
@@ -21,6 +31,71 @@ jQuery.noConflict();
             ];
         }
         console.log(confValue);
+
+        var param = {'app': kintone.app.getId()};
+        var url = kintone.api.url('/k/v1/preview/app/form/fields', true);
+        // アプリのフォームフィールド情報を取得
+        kintone.api(url, 'GET', param, function(resp) {
+            var cnt = 0;
+            // tr１行目の要素のクローンを変数にセット
+            var $cloneElem = $('.zero-fill-plugin_div').clone();
+            // var $cloneElem = $('.prt-plugin_tr').clone();
+            console.log($cloneElem);
+            // フィールド要素ごとループ
+            for (var key in resp.properties) {
+                // keyが存在しない場合は次へ
+                if (!resp.properties.hasOwnProperty(key)) { continue; }
+                // フィールド要素１つ取り出し
+                var prop = resp.properties[key];
+                // フィールドタイプごとに処理を分ける
+                switch (prop.type) {
+                    case 'SUBTABLE':
+                    case 'STATUS':
+                    case 'STATUS_ASSIGNEE':
+                    case 'RECORD_NUMBER':
+                    case 'CALC':
+                        break;
+                    
+                    case 'NUMBER':
+                        if (cnt !== 0) {
+                            // ２行目以降は、要素をクローンして追加
+                            // $cloneElem.clone().appendTo('#prt-plugin-table');
+                            $cloneElem.clone().appendTo('#form');
+                        }
+
+                        $('.label').eq(cnt).text(escapeHtml(prop.label));
+                        $('.checkbox').eq(cnt).attr('id', 'check_' + cnt);
+                        $('.checkbox').eq(cnt).attr('value', escapeHtml(prop.code));
+
+                        // 設定画面のテーブルの列ループ
+                        // for (var i = 0; i < 3; i++) {
+                        //     switch (i) {
+                        //         case 0:
+                        //             // タイトル
+                        //             $('.prt-plugin-td_title').eq(cnt).text(escapeHtml(prop.label));
+                        //             break;
+                        //         case 1:
+                        //         case 2:
+                        //             // checkboxに id と value をセット
+                        //             $('.checkbox_' + i).eq(cnt).attr('id', 'check_' + cnt + '-' + i);
+                        //             $('.checkbox_' + i).eq(cnt).attr('value', escapeHtml(prop.code));
+                        //             // 既存の設定値を反映
+                        //             if ($.inArray(prop.code, confValue[i - 1]) !== -1) {
+                        //                 $('.checkbox_' + i).eq(cnt).attr('checked', 'checked');
+                        //             }
+                        //             // label に checkbox の id を関連付ける
+                        //             $('.label_' + i).eq(cnt).attr('for', 'check_' + cnt + '-' + i);
+                        //             break;
+                        //     }
+                        // }
+
+                        // default :
+
+                        cnt++;
+                        break;
+                }
+            }
+        });
 
         function createConfig() {
             // 項目にチェックがついているフィールドを取得
