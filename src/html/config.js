a98dc6135b642ffd9290ce097493a6654eef093a
Kazuhiro Yoshida <sy250f@gmail.com>
2019-02-11 22:25:26 +0900

    change config.js

--- src/html/config.js
+++ src/html/config.js
@@ -63,13 +63,15 @@ jQuery.noConflict();
                             $cloneElem.clone().appendTo('#form');
                         }
 
-                        $('.label').eq(cnt).text(escapeHtml(prop.label));
+                        // $('.label').eq(cnt).text(escapeHtml(prop.label));
+                        $('.label').eq(cnt).text(escapeHtml(prop.code));
+                        $('.checkbox').eq(cnt).attr('name', escapeHtml(prop.code));
                         $('.checkbox').eq(cnt).attr('id', 'check_' + cnt);
                         $('.checkbox').eq(cnt).attr('value', escapeHtml(prop.code));
 
                         // 既存の設定値を反映
                         if ($.inArray(prop.code, confValue[0]) !== -1) {
-                            $('.checkbox_' + i).eq(cnt).attr('checked', 'checked');
+                            $('.checkbox').eq(cnt).attr('checked', 'checked');
                         }
 
                         // 設定画面のテーブルの列ループ
